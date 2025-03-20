74314cb978043f2637e58987f6c75ed6683dfa76
abhishek-maheswari-bangaru <abhishek.bangaru.1802@gmail.com>
2025-03-21 00:02:29 +0530

    Enabled server run from port 5000, added routing logic, added basic controller

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const port = 5000;
+
+const app = express();
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/todos', require('./backend/routes/todoRoutes'))
+
+app.listen(port, ()=> {
+    console.log(`Server is listening on port ${port}`);
+});
+
