9e46aeb58e70d66ccf24c9d81ee15d5f7c55d021
abhishek-maheswari-bangaru <abhishek.bangaru.1802@gmail.com>
2025-03-23 23:32:52 +0530

    Established connection with MongoDB

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,24 +1,24 @@
 const asyncHandler = require('express-async-handler');
 
-const getTodo = (request, response) => {
+const getTodo = asyncHandler(async (request, response) => {
     response.set(200).json({message: 'Getting Todos', });
-}
+})
 
-const setTodo = (request, response) => {
+const setTodo = asyncHandler(async (request, response) => {
     if(!request.body.text) {
         response.status(400);
         throw new Error('Please add a text in the response');
     }
     response.set(200).json({message: 'Setting Todos'});
-}
+})
 
-const updateTodo = (request, response) => {
+const updateTodo = asyncHandler(async (request, response) => {
     response.set(200).json({message: 'Updating Todos'});
-}
+})
 
-const deleteTodo = (request, response) => {
+const deleteTodo = asyncHandler(async (request, response) => {
     response.set(200).json({message: 'Deleting Todos'});
-}
+})
 
 module.exports = {
     getTodo,
