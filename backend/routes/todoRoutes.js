f70cb76e48f0ce3baa0ba42de7294ab217590b4e
abhishek-maheswari-bangaru <abhishek.bangaru.1802@gmail.com>
2025-04-04 05:27:11 +0530

    Protected routes using Newly created middleware

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { protect } = require('../middleware/authorizationMiddleware');
 const { getTodo, setTodo, updateTodo, deleteTodo } = require('../controllers/todoController');
 
-router.route('/').get(getTodo).post(setTodo)
-router.route('/:id').put(updateTodo).delete(deleteTodo)
+router.route('/').get(protect, getTodo).post(protect, setTodo)
+router.route('/:id').put(protect, updateTodo).delete(protect, deleteTodo)
 
 module.exports = router;
