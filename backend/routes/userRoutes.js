a3131d3fdc0fefbcf9a7a30f3e6849c20763be65
abhishek-maheswari-bangaru <abhishek.bangaru.1802@gmail.com>
2025-04-03 06:00:16 +0530

    Included authorization middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, getLoggedInUser } = require('../controllers/userController');
+const { protect } = require('../middleware/authorizationMiddleware');
 
 router.route('/').post(registerUser);
 router.route('/login').post(loginUser);
-router.route('/me').post(getLoggedInUser);
+router.get("/me", protect, getLoggedInUser);
 
 module.exports = router;
